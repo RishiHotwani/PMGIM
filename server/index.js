27e72914c74f44a0bc7d3a5f5962aaa3759b61d9
Rishi Hotwani <rishiii787@gmail.com>
2026-08-10 10:37:59 +0530

    Fix missing closing parenthesis on res.json in server/index.js

--- server/index.js
+++ server/index.js
@@ -703,6 +703,7 @@ app.post('/api/explore/:id/reviews', async (req, res, next) => {
       success: true,
       message: 'Review and rating submitted successfully!',
       id: result.insertId
+    });
   } catch (err) {
     next(err);
   }
